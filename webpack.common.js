c1c5af0ecdee3763ee6321a3ef51bd2e99d0452b
Andrej Amelyanovich <amelyanovich.andrej@mail.ru>
2023-07-04 14:25:02 +0300

    feat: init task

---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    cache: {
+        type: 'filesystem',
+    },
+    plugins: [
+        new HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Horizont test task',
+            template: './public/index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|ts)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                },
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.*', '.js', '.jsx', '.ts', '.tsx'],
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+};
